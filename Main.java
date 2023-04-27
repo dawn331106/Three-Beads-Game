8b47ff17767500386dfa218fe7035e4758914790
Prato Dewan <56154287+dawn331106@users.noreply.github.com>
2023-04-27 07:45:05 +0600

    Comments added

--- Main.java
+++ Main.java
@@ -1,9 +1,14 @@
-
-package Intro;
+package "Package Name";
 
 public class Main {
     public static void main(String args[])
     {
-        GameWindow1 Thegame=new GameWindow1();
+        /*
+        * The Game will start by running this file.
+        * It will open the menu with two options -
+        *  1) "New Game"
+        *  2) "Exit"
+        */
+        Menu start=new Menu();
     }
 }
